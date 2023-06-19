ccb0fbdf310e8c10dc4eaf62da71dc216fbdd0c6
Valdenir Roman Junior <vromanjunior@outlook.com>
2023-06-19 09:10:50 -0300

    correção60 18/06

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,5 +1,5 @@
  import axios from 'axios';
-  const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://baseview-api.herokuapp.com";
+  const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? " https://baseview-api.herokuapp.com";
 
 
 
