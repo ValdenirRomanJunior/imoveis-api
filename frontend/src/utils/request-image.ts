f14a9790bd078b954e2e1541d51dcf535ba17a54
Valdenir Roman Junior <vromanjunior@outlook.com>
2023-06-20 09:39:55 -0300

    correção63 19/06

--- frontend/src/utils/request-image.ts
+++ frontend/src/utils/request-image.ts
@@ -1,13 +1,13 @@
  import axios from 'axios';
   
-  export const BASE_URL_FROM_BUCKET = process.env.REACT_APP_BACKEND_URL_PROFILE ?? "https://dynamous.s3.sa-east-1.amazonaws.com/";
+  export const BASE_URL_FROM_BUCKET = "https://dynamous.s3.sa-east-1.amazonaws.com/";
 
 const apiImage= axios.create({
     baseURL:BASE_URL_FROM_BUCKET,
     headers:{
      
         'Access-Control-Allow-Headers': 'Content-Type',
-        "Access-Control-Allow-Origin": "http:localhost:3000",
+        "Access-Control-Allow-Origin": "https://master--steady-cheesecake-480a84.netlify.app",
         "Access-Control-Allow-Methods": "OPTIONS,POST,GET" 
         
         
