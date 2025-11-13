8a4c4cf46ca8a1405a31bcfe8aed0835cddade8b
Valdenir Roman Junior <vromanjunior@outlook.com>
2025-11-13 18:26:58 -0300

    bugsssssss

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -169,7 +169,7 @@ const Dashboard = ()=>{
           
         <div>
        
-        <ErrorBoundary FallbackComponent={Dashboard}>
+        <ErrorBoundary FallbackComponent={PageNotFoundDashboard}>
          {!errors ?    
         <DashboardBackground>
                  
